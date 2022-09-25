e9f6c4806f18a97f85cf834b96ed20327da3c1d6
KhayneGleave <114017293+KhayneGleave@users.noreply.github.com>
2022-09-25 11:28:52 +0100

    Update GetToken.js

--- RobloxAPI/Modules/Util/GetToken.js
+++ RobloxAPI/Modules/Util/GetToken.js
@@ -13,7 +13,7 @@ exports.func = async() => {
 
         }, async (error, response, body) => {
 
-            resolve(response.headers['x-csrf-token']) //Returns token to exports.func.
+            resolve(response.headers['x-csrf-token']) //Returns token to callingscript.
 
         })
 
